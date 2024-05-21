b47a6b426cd322689651a12913c1fc058ca4b27b
arreejj <areej2102776@miuegypt.edu.eg>
2024-05-22 02:04:42 +0300

    contollercheckout

---
+++ src/main/java/com/example/blissful/blissful/controllers/CheckoutController.java
@@ -0,0 +1,21 @@
+package com.example.blissful.blissful.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.blissful.blissful.models.BillingInfo;
+
+@Controller
+public class CheckoutController {
+
+    @GetMapping("/checkout")
+    public String showForm(Model model) {
+        model.addAttribute("billingInfo", new BillingInfo());
+        return "checkout";
+    }
+
+   
+}
