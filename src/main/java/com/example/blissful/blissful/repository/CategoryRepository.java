2ad4e766162ff4fb4647fbe45e9078e4ce6a50b3
shahd soliman <shahd2101760@miuegypt.edu.eg>
2024-05-24 21:58:38 +0300

    repo category

--- src/main/java/com/example/blissful/blissful/repository/CategoryRepository.java
+++ src/main/java/com/example/blissful/blissful/repository/CategoryRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.blissful.blissful.models.Category;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-  
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByName(String name);
+
 }
