4b3330e84b67b8182953be84487f701ed24fa6d8
shahd soliman <shahd2101760@miuegypt.edu.eg>
2024-05-29 00:15:24 +0300

    aspect

--- src/main/java/com/example/blissful/blissful/logging/UserLogging.java
+++ src/main/java/com/example/blissful/blissful/logging/UserLogging.java
@@ -20,42 +20,6 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
-// @Aspect
-// @Component
-// public class UserLogging {
-
-//     @Autowired
-//     private UserLogRepository userLogRepository;
-
-//     @Before("execution(* com.example.blissful.blissful.controllers.usercontroller.*(..))")
-//     public void beforeUserControllerMethod(JoinPoint joinPoint) {
-//         // Extract information from the JoinPoint
-//         String methodName = joinPoint.getSignature().getName();
-//         String className = joinPoint.getTarget().getClass().getSimpleName();
-
-//         // Log the method name and class name
-//         System.out.println("Method Executed: " + methodName + " in class: " + className);
-
-//         // Extract information from the session
-//         HttpSession session = ((ServletRequestAttributes) RequestContextHolder
-//                 .currentRequestAttributes()).getRequest().getSession();
-//         String username = (String) session.getAttribute("username");
-
-//         // Extract information from the request
-//         jakarta.servlet.http.HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-//                 .currentRequestAttributes()).getRequest();
-//         String pageVisited = request.getRequestURI();
-//         Date loginTime = new Date();
-
-//         // Save user log to the database
-//         UserLog userLog = new UserLog();
-//         userLog.setUserId(username);
-//         userLog.setLoginTime(loginTime);
-//         userLog.setPageVisited(pageVisited);
-//         userLogRepository.save(userLog);
-//     }
-
-// }
 @Aspect
 @Component
 public class UserLogging {
@@ -66,7 +30,7 @@ public class UserLogging {
     @Autowired
     private userrepo userRepository; // Assuming you have a UserRepository
 
-    @Before("execution(* com.example.blissful.blissful.controllers.*.*(..)) && !execution(* com.example.blissful.blissful.controllers.usercontroller.login(..))")
+    @Before("execution(* com.example.blissful.blissful.controllers.*.*(..)) ")
     public void beforeUserControllerMethod(JoinPoint joinPoint) {
         // Extract information from the JoinPoint
         String methodName = joinPoint.getSignature().getName();
