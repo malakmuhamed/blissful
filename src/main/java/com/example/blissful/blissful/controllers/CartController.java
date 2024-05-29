c63f5f1f84a6039e242238473cfb00bea6acedef
malakhelal <malak2102056@miuegypt.edu.eg>
2024-05-29 04:24:35 +0300

    7

--- src/main/java/com/example/blissful/blissful/controllers/CartController.java
+++ src/main/java/com/example/blissful/blissful/controllers/CartController.java
@@ -1,14 +1,25 @@
 package com.example.blissful.blissful.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.example.blissful.blissful.models.cart;
-@Controller
+import com.example.blissful.blissful.models.Cart;
+import com.example.blissful.blissful.models.CartItem;
+import com.example.blissful.blissful.service.CartService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
 
     @GetMapping("/cart")
     public String showForm(Model model) {
